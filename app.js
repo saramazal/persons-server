b5de8c543802286c88bec7f6aa63dd72472e41e2
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-03-13 22:58:19 +0200

    server

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-const express = require('express');
-require('./db')
-const cors = require('cors'); // import the CORS library to allow Cross-origin resource sharing
-const app = express(); // create an instance of the express module (app is the conventional variable name used)
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
 
-const router = require('./router');
+app.use(express.json())
+
+const router = require('./router')
 router(app)
 
 const PORT = process.env.PORT || 5000;
 
-
-app.use(cors()); // Enable CORS 
-app.use(express.json()); // Recognize Request Objects as JSON objects
-app.use(express.static('public')); // serve static files (css & js) from the 'public' directory
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
--- db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({
-    path: './.env'
-})
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.CONNECTION_STRING)
-    .then(() => console.log('Connect Wel Done!'))
