57246f666f7007e83a28209179a9439fb4ce564b
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-03-13 21:20:32 +0200

    ädding mogoose

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express'); // import express module (simplifies routing/requests, among other things)
+const express = require('express');
+require('./db')
 const cors = require('cors'); // import the CORS library to allow Cross-origin resource sharing
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
 const bodyParser = require('body-parser');
@@ -13,7 +14,9 @@ app.use(cors()); // Enable CORS
 app.use(express.json()); // Recognize Request Objects as JSON objects
 app.use(express.static('build')); // serve static files (css & js) from the 'public' directory
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 
 app.listen(PORT, () => {
